756d369f9a89609d7bc2d07f4910bda2e8529f51
minsoo9506 <ghktjd15gh@gmail.com>
2022-06-18 20:32:09 +0900

    feat: api 만드는  코드 작성

---
+++ src/service.py
@@ -0,0 +1,69 @@
+import json
+
+import bentoml
+import numpy as np
+import pandas as pd
+from bentoml.io import JSON, NumpyNdarray
+
+# Load processors and model
+
+
+class scalerRunnable(bentoml.Runnable):
+    """_summary_
+
+    Parameters
+    ----------
+    bentoml : _type_
+        _description_
+    """
+
+    def __init__(self):
+        self.model = bentoml.sklearn.load_model("scaler:latest")
+
+    @bentoml.Runnable.method(batchable=False)
+    def transform(self, input_data) -> np.ndarray:
+        """_summary_
+
+        Parameters
+        ----------
+        input_data : _type_
+            _description_
+
+        Returns
+        -------
+        np.ndarray
+            _description_
+        """
+        return self.model.transform(input_data)
+
+
+scaler = bentoml.Runner(scalerRunnable)
+classifier = bentoml.sklearn.get("model:latest").to_runner()
+
+# Create service
+service = bentoml.Service("fraud_detection", runners=[scaler, classifier])
+
+
+@service.api(input=JSON(), output=NumpyNdarray())
+def predict(input_json: JSON) -> np.ndarray:
+    """_summary_
+
+    Parameters
+    ----------
+    input_json : JSON
+        _description_
+
+    Returns
+    -------
+    np.ndarray
+        _description_
+    """
+
+    df = pd.DataFrame(input_json, index=[0])
+
+    # Process data
+    scaled_df = pd.DataFrame(scaler.run(df), columns=df.columns)
+
+    # Predict
+    result = classifier.run(scaled_df)
+    return np.array(result)
